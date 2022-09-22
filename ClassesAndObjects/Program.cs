03c1b1ce7721bfb3b2a88f575c33765fe90ffcd1
Ben Cofield <benrcofield@gmail.com>
2022-09-22 14:46:58 -0400

    First commit

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Transactions;
+using Cars;
+
+Console.WriteLine("Enter the make, model, and year of your car:");
+var newCar = new Car()
+{
+    Make = Console.ReadLine(),
+    Model = Console.ReadLine(),
+    Year = int.Parse(Console.ReadLine())
+};
+Console.WriteLine($"You have a {newCar.Year} {newCar.Make} {newCar.Model}");
